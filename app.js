26118e6a7dc55c864bd9c18544be0b4eaf522cf4
mgmavely <mgmavely@gmail.com>
2021-03-03 17:48:46 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const app = express();
+var items = ["Buy Food", "Cook Food", "Eat Food"];
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+
+  var today = new Date();
+  var currentDay = today.getDay();
+  var day = "";
+
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  }
+
+  var day = today.toLocaleDateString("en-US", options)
+
+  res.render("list", {
+    kindOfDay: day,
+    newListItems: items
+  });
+});
+
+app.post("/", function(req, res) {
+  var item = req.body.userInput;
+  items.push(item);
+  res.redirect("/")
+});
+
+
+
+
+
+app.listen(3000, function() {
+  console.log("Sever started on port 3000");
+});
